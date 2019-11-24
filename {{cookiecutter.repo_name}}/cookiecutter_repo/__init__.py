df9ad80b433ec83993289435652d63f3d126ae06
prem <prem@cortex.cs.northwestern.edu>
2019-11-24 14:13:40 -0600

    moving logging config to init

--- {{cookiecutter.repo_name}}/cookiecutter_repo/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import algorithms
-from . import dataset
-from . import model
-
-from .logger import logging
--- {{cookiecutter.repo_name}}/cookiecutter_repo/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(
-    format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-    datefmt='%Y-%m-%d:%H:%M:%S',
-    level=logging.INFO
-)
