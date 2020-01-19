edec018c60bece5059388e1da70e3a0967f30d7d
pseeth <prem@u.northwestern.edu>
2020-01-19 01:59:50 +0000

    separating into steps...hopefully works

--- {{ cookiecutter.repo_name }}/tests/__init__.py
+++ {{ cookiecutter.repo_name }}/tests/__init__.py
@@ -2,6 +2,11 @@ import comet_ml
 
 import os
 import sys
+import pytest
 
 from scripts.download_toy_data import download_toy_data
 download_toy_data('tests/out/_test_data/')
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_tests():
+    download_toy_data()
